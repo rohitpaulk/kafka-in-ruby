5be7e6694b3a7b68dea35f86572c6b524bd8c8f9
Paul Kuruvilla <rohitpaulk@gmail.com>
2024-07-24 14:11:26 +0100

    Refactor test file to fix require path and update test case for encoding request header

--- test/requests/api_versions_request_v2_test.rb
+++ test/requests/api_versions_request_v2_test.rb
@@ -1,9 +1,9 @@
-require_relative "test_helper"
+require_relative "../test_helper"
 
 class ApiVersionsRequestV2Test < Minitest::Test
   def test_encode
-    request_header = RequestHeader.new(api_key: 18, api_version: 2, correlation_id: 1, client_id: 1)
+    request_header = RequestHeader.new(api_key: 18, api_version: 2, correlation_id: 1)
     request = Requests::ApiVersionsRequestV2.new(header: request_header)
-    assert_equal "\x00\x00\x00\x2A", request.encode
+    assert_equal "abcd", request.encode
   end
 end
